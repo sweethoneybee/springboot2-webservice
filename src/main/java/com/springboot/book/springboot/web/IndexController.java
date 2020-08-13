11ff2eef173135682b6309d226d9a1fa433a4966
jsjphone8 <jsjphone8@gmail.com>
2020-08-13 23:16:10 +0900

    게시글 등록 화면 구현

---
+++ src/main/java/com/springboot/book/springboot/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.springboot.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
+    }
+}
